833d68af5644b9e12571087e2b67d0d185e5d681
Pacôme MARTIN <pacome.martin@hotmail.fr>
2023-09-21 17:48:19 +0200

    Add a function to display each book on the page

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read ? "read" : "not read yet";
+    this.read = read ? "Read" : "Not read";
     this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
     };
@@ -15,21 +15,64 @@ function Book(title, author, pages, read) {
 
 //Takes user's input and store the new book objects into an array (myLibrary) 
 function addBookToLibrary() {
-    //e.preventDefault(); // to stop the form for submitting
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector("#author").value;
-    const pages = document.querySelector("#pages").value;
-    const read = document.querySelector("#read").checked;  
-    const book = new Book(title, author, pages, read);
+  //e.preventDefault(); // to stop the form for submitting
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+  const book = new Book(title, author, pages, read);
 
-    myLibrary.push(book);
-    document.querySelector('form').reset();
-    
-    //for display purposes only
-    //console.warn('added', book);
-    //console.log(myLibrary);
-    //return myLibrary;
+  myLibrary.push(book);
+  document.querySelector("form").reset();
+
+  //for display purposes only
+  //console.warn('added', book);
+  console.log(myLibrary);
+
+  //Create a card and add it to the div for display on the page
+  myLibrary.forEach((Book) => {
+    if (Book.title !== "" || Book.author !== "" || Book.pages !== "") {
+      //Create a card and add css style to the card
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      //Wrap the book title within an h2 and append it to the card
+      const cardTitle = document.createElement("h2");
+      cardTitle.textContent = Book.title;
+      card.appendChild(cardTitle);
+
+      //Embed the book author within a p and append it to the card
+      const cardAuthor = document.createElement("p");
+      cardAuthor.textContent = `by ${Book.author}`;
+      card.appendChild(cardAuthor);
+
+      //Embed the number of pages within a p and append it to the card
+      const cardPages = document.createElement("p");
+      cardPages.textContent = `${Book.pages} pages`;
+      card.appendChild(cardPages);
+
+      //Create a button for having read or not read the book and add it to the card
+      const cardRead = document.createElement("button");
+      cardRead.textContent = Book.read;
+      cardRead.classList.add("progress");
+      card.appendChild(cardRead);
+
+      //create a button to delete the card and append it to the card
+      const cardDelete = document.createElement("button");
+      cardDelete.textContent = "Delete";
+      cardDelete.classList.add("delete");
+      card.appendChild(cardDelete);
+
+      // Append the card to the display div on the page
+      const display = document.querySelector(".display");
+      display.appendChild(card);
+    }
+  });
+
+  myLibrary.pop(book);
 }
+
+
 
 
 // Toggle the modal dialog
@@ -40,6 +83,7 @@ const openModal = document.querySelector('.openModal');
 openModal.addEventListener('click', () => {
     modal.showModal();
 });
+
 /*
 closeModal.addEventListener('click', () => {
     modal.close();
@@ -51,10 +95,6 @@ modal.addEventListener('click', (e) => {
         modal.close();
     };
 });
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#addBtn').addEventListener('click', addBookToLibrary);
